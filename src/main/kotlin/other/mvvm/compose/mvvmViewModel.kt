f0dc7d2d81dd675a69431d542a4ecafd9d682898
eternel <1012435210@qq.com>
2024-03-19 14:35:47 +0800

    update BaseViewModel

--- src/main/kotlin/other/mvvm/compose/mvvmViewModel.kt
+++ src/main/kotlin/other/mvvm/compose/mvvmViewModel.kt
@@ -6,24 +6,16 @@ fun mvvmViewModel(packageName:String,
 package ${packageName}.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.wls.base.entity.ResultState
+import com.wls.base.BaseViewModel
 import ${packageName}.repository.${composeClass}Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class ${composeClass}ViewModel @Inject constructor(private val repository: ${composeClass}Repository) :
-    ViewModel() {
-private val _state = MutableSharedFlow<ResultState<Any>>()
-    val state = _state.stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(5000),ResultState.None
-    )
- 
-      
+    BaseViewModel<Unit>() {
+
+     
 }
 
                     """
